07fe38c0b1f07ce2e08f721d2761531a84c3b344
Senorariapardede <48901159+Senorariapardede@users.noreply.github.com>
2019-05-06 07:31:38 +0700

    connection database

---
+++ tubeslokeritera/TubesLokerItera.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubeslokeritera;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TubesLokerItera {
+
+
+    public static void main(String[] args)  {
+        // TODO code application logic here
+        
+    
+    }
+}
