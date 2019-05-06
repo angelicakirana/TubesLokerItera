55391c3d626ce4662bd0cc4bd70f81e7c1baf551
Senorariapardede <48901159+Senorariapardede@users.noreply.github.com>
2019-05-06 09:18:08 +0700

    connect database

---
+++ src/tubeslokeritera/koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubeslokeritera;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class koneksi {
+    private static Connection koneksi;
+    public static Connection getKoneksi (){
+        if(koneksi==null){
+            try{
+                String url;
+                url="jdbc:mysql://localhost:3306/tubeslokeritera";
+                String username="root";
+                String password=" ";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, username, password);
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(null, "Connection Error");
+            }
+        }return koneksi;
+    }static Object getConnection(){
+        throw new UnsupportedOperationException("Not yet implementated");
+    }
+}
